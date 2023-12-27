2446c7bd5450f0d47dbedf9a4faed2b26c5d712e
md-abdul-ahammed <mdabdulahammed1@gmail.com>
2023-12-28 03:16:09 +0600

    fix-phone-input-issue

--- src/components/Form/FormPhoneInput.tsx
+++ src/components/Form/FormPhoneInput.tsx
@@ -1,96 +1,24 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Button, Input, InputRef, Space } from "antd";
-import { CountrySelector, usePhoneInput } from "react-international-phone";
-import "react-international-phone/style.css";
-import { useFormContext, Controller } from "react-hook-form";
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { AntPhoneInput } from "./AntPhoneInput/AntPhoneInput";
 
-interface CombinedAntFormPhoneInputProps {
+interface FormAntPhoneProps {
   name: string;
   label?: string;
-  size?: "large" | "small";
-  value: string;
-  onChange: (phone: string) => void;
 }
 
-const FormPhoneInput: React.FC<CombinedAntFormPhoneInputProps> = ({
-  name,
-  label,
-  size,
-  value,
-  onChange,
-}) => {
+const FormPhoneInput: React.FC<FormAntPhoneProps> = ({ name, label }) => {
   const { control } = useFormContext();
-
-  const phoneInput = usePhoneInput({
-    defaultCountry: "bd",
-    value: value,
-    onChange: (data) => {
-      onChange(data.phone);
-    },
-  });
-
-  const inputRef = useRef<InputRef>(null);
-  const [cursorPosition, setCursorPosition] = useState<number | null>(null);
-
-  useEffect(() => {
-    if (inputRef.current?.input && cursorPosition !== null) {
-      inputRef.current.input.setSelectionRange(cursorPosition, cursorPosition);
-    }
-  }, [cursorPosition]);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const previousValue = phoneInput.phone;
-    phoneInput.handlePhoneValueChange(e);
-    const lengthDifference = phoneInput.phone.length - previousValue.length;
-    setCursorPosition((prevCursorPosition) => {
-      return prevCursorPosition !== null
-        ? prevCursorPosition + lengthDifference
-        : null;
-    });
-  };
 
   return (
     <>
-      {label && <span>{label}</span>}
+      {label ? <span>{label}</span> : null}
       <Controller
         control={control}
         name={name}
         render={({ field }) => {
-          console.log(field);
           return (
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <Space.Compact className="w-full">
-                <CountrySelector
-                  selectedCountry={phoneInput.country.iso2}
-                  onSelect={(country) => phoneInput.setCountry(country.iso2)}
-                  renderButtonWrapper={({ children, rootProps }) => (
-                    <Button
-                      {...rootProps}
-                      style={{
-                        padding: "4px",
-                        height: "100%",
-                        zIndex: 1, // fix focus overlap
-                      }}
-                    >
-                      {children}
-                    </Button>
-                  )}
-                  dropdownStyleProps={{
-                    style: {
-                      top: "35px",
-                    },
-                  }}
-                />
-                <Input
-                  placeholder="Phone number"
-                  type="tel"
-                  size={size}
-                  value={phoneInput.phone}
-                  onChange={handleInputChange}
-                  ref={inputRef}
-                />
-              </Space.Compact>
-            </div>
+            <AntPhoneInput value={field.value} onChange={field.onChange} />
           );
         }}
       />
